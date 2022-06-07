9c903aa13f25703a929ca89547587bb9ccbc3a3b
deividgaliani <deividgaliani@gmail.com>
2022-06-07 12:53:33 -0300

    first commit

---
+++ src/main/java/com/example/squarematrixrotationapi/SquareMatrixRotationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.squarematrixrotationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareMatrixRotationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareMatrixRotationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/squarematrixrotationapi/SquareMatrixRotationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squarematrixrotationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareMatrixRotationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
